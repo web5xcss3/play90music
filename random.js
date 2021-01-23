bf866ab686465501e2dfd0c9c001309d059484e1
web5xcss3 <web5xcss3@gmail.com>
2021-01-23 20:07:26 -0300

    Delete random.js

--- random.js
+++
@@ -1,1 +0,0 @@
-window.onload=function(){var E=document.getElementsByClassName("inner");var m=E.length;var n=parseInt(Math.random()*m);for(var i=m-1;i>=0;i--){var e=E[i];e.style.display='none';}E[n].style.display='';}
